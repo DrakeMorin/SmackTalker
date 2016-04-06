3edb6a16fa201c733e4671fe8062628deb67515d
Drake LaRiviere <drake.lariviere@gmail.com>
2016-04-06 08:37:33 -0400

    Add variable and setter method which determines what numerical value represents the first char in senderID. This will be used for setting custom images based on which char it is.

--- app/src/main/java/com/example/drake/listviewtest/MessageData.java
+++ app/src/main/java/com/example/drake/listviewtest/MessageData.java
@@ -1,8 +1,6 @@
 package com.example.drake.listviewtest;
 
-/**
- * Created by Drake on 2016-03-08.
- */
+
 class MessageData {
 
     //Objects of this class will be used to hold the text messages sent and received
@@ -16,18 +14,76 @@ class MessageData {
     //Holds name of sender
     String senderID;
 
+    //Holds the ID of the img to be shown based on the first char in senderID
+    int imgID;
+
+    int[] myImageArray = new int[]{
+            R.drawable.me,
+            R.drawable.you,
+            R.drawable.me,
+            R.drawable.you,
+            R.drawable.me,
+            R.drawable.you,
+            R.drawable.me,
+            R.drawable.you,
+            R.drawable.me,
+            R.drawable.you,
+            R.drawable.me,
+            R.drawable.you,
+            R.drawable.me,
+            R.drawable.you,
+            R.drawable.me,
+            R.drawable.you,
+            R.drawable.me,
+            R.drawable.you,
+            R.drawable.me,
+            R.drawable.you,
+            R.drawable.me,
+            R.drawable.you,
+            R.drawable.me,
+            R.drawable.you,
+            R.drawable.me,
+            R.drawable.you,
+    };
+
     public MessageData(String message, String time, String senderID) {
         //Constructor that sets data
         this.message = message;
         this.time = time;
         this.senderID = senderID;
+        this.imgID = parseSenderID();
+    }
+
+    private int parseSenderID(){
+        //This method determines what the first char of senderID is, and assigns an int
+        //to it based on what letter it is (0 - 26). A is 0, Z is 25.
+        //Special letters are all assigned 26
+
+        //Returns the first char of the senderID for the message.
+        char senderChar = senderID.toLowerCase().charAt(0);
+        //This code determines what number the char is as part of the alphabet.
+        int charID;
+        //This should avoid crashes when the first char in the senderID is not a letter.
+        if(Character.isLetter(senderChar)) {
+            int temp = (int) senderChar;
+            //Note the 96 is a constant for lower case.
+            if (temp <= 122 & temp >= 97) {
+                charID = (temp - 96 - 1);
+            } else {
+                charID = temp - 1;
+            }
+            //The -1 for the charID is to allow for array indexes which start at 0, not 1.
+        }else{
+            charID = 26;
+        }
+
+        //Return the image id value.
+        return myImageArray[charID];
     }
 
     //GETTER METHODS
 
-    public String getMessage() {
-        return message;
-    }
+    public String getMessage() { return message; }
 
     public String getTime() {
         return time;
@@ -36,4 +92,6 @@ class MessageData {
     public String getSenderID() {
         return senderID;
     }
+
+    public int getImgID() { return imgID; }
 }
