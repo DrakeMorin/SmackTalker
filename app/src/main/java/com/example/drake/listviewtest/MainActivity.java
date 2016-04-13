a9f12f87702c61b8bb8c31abf8fa45a738624ccd
DavidRileyy <david.riley203@gmail.com>
2016-04-13 08:56:15 -0400

    fixed it lol

--- app/src/main/java/com/example/drake/listviewtest/MainActivity.java
+++ app/src/main/java/com/example/drake/listviewtest/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.drake.listviewtest;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.format.DateFormat;
-import android.text.format.Time;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -13,34 +16,37 @@ import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
 
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     protected final static String DEBUGTAG = "DED";
     public final String FILENAME = "SmackTalkerMessages.ded";
-    protected String userID = "Bob";
+    protected ArrayList<MessageData> messages;
+    protected String userID;
+    private BluetoothAdapter btAdapter;
+    private Button Bluetooth;
 
-    myDBHandler dbHandler;
 
     EditText newMessageText;
     ListAdapter myListAdapter;
     ListView listView;
+    myDBHandler dbHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         //Since the last three parameters are constants of the class, null is passed.
         dbHandler = new myDBHandler(this, null, null, 1);
@@ -65,17 +71,56 @@ public class MainActivity extends AppCompatActivity {
         populateListView();
     }
 
+    //INFLATES ACTION BAR
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        Log.d(DEBUGTAG, "Options Menu Inflated");
+        return true;
+    }
+
+
+    //CHECKS FOR IF ANY OF THE ITEMS IN THE ACTION BAR ARE PRESSED
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        if(item.getItemId() == R.id.action_bluetooth){
+            Log.d(DEBUGTAG, "Bluetooth button pressed");
+            btButtonClick();
+        }
+        return true;
+    }
+
+
+    //IF BLUETOOTH BUTTON IS CLICKED, TURN ON/OFF BLUETOOTH AND ALERT THE USER
+    public void btButtonClick() {
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        //BLUETOOTH IS ALREADY ON, TOAST THE USER
+        if(btAdapter.isEnabled()){
+            String address = btAdapter.getAddress();
+            String name = btAdapter.getName();
+            String statusText = name + ":" + address;
+
+            Toast toast = new Toast(getApplicationContext());
+            toast.setGravity(Gravity.BOTTOM| Gravity.LEFT, 0, 0);
+            toast.makeText(MainActivity.this, "Bluetooth Already On: " + statusText, toast.LENGTH_LONG).show();
+
+        }
+
+    }
+
+
+
+
     //Message is ready to be sent.
     public void sendButtonClicked(View view){
         if (!newMessageText.getText().toString().equals("")) {
             //Only run if newMessageText is not empty
-
-            //Intialize a calendar to current date
-            Calendar c = Calendar.getInstance();
-            //Create format for the date
-            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            //Format the date and set it to a string
-            String timeStamp = df.format(c.getTime());
+            //calendar.getInstance();
+            //String timeStamp = calendar.toString();
+            String timeStamp = "0";
+            //Aside: Format "%Y-%m-%d %H:%M:%S"
 
             //Add to database a new MessageData object with fields.
             dbHandler.addMessage(new MessageData(newMessageText.getText().toString(), timeStamp, userID));
@@ -94,9 +139,9 @@ public class MainActivity extends AppCompatActivity {
     private void populateListView(){
         Cursor myCursor = dbHandler.getAllRows();
         //What data you are going to populate the data with
-        String [] fromFieldNames = new String[] {myDBHandler.COLUMN_MESSAGETEXT, myDBHandler.COLUMN_SENDERID, myDBHandler.COLUMN_TIME, myDBHandler.COLUMN_IMGID};
+        String [] fromFieldNames = new String[] {myDBHandler.COLUMN_MESSAGETEXT, myDBHandler.COLUMN_SENDERID, myDBHandler.COLUMN_TIME};
         //Where the data is going to go.
-        int[] toViewIDs = new int[] {R.id.listRowMessage, R.id.listRowSender, R.id.listRowTime, R.id.listRowImage};
+        int[] toViewIDs = new int[] {R.id.listRowMessage, R.id.listRowSender, R.id.listRowTime};
 
         //Define cursorAdapter, instantiated next line.
         SimpleCursorAdapter myCursorAdapter;
@@ -108,12 +153,11 @@ public class MainActivity extends AppCompatActivity {
 
         //Sets listView adapter to the cursorAdapter
         myListView.setAdapter(myCursorAdapter);
+
     }
 
     //For testing purposes.
-    public void testButtonClicked(View view){
-        userID = newMessageText.getText().toString();
-        newMessageText.setText("");
-        Log.d(DEBUGTAG, "userID updated");
+    public void addButtonClicked(View view){
+        Log.d(DEBUGTAG, "Temp");
     }
 }
