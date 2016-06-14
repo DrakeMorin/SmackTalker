347148f0da5734c478ee9917678ce0d1a09c50b3
Drake LaRiviere <drake.lariviere@gmail.com>
2016-06-14 08:59:37 -0400

    Remove superfluous import statements in MainActivty

--- app/src/main/java/com/ded/smacktalker/MainActivity.java
+++ app/src/main/java/com/ded/smacktalker/MainActivity.java
@@ -1,50 +1,9 @@
 package com.ded.smacktalker;
 
-import android.app.NotificationManager;
-import android.app.PendingIntent;import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.content.Context;
-import android.content.ClipData;
-import android.content.ClipboardManager;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.SharedPreferences;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteCursor;
-import android.os.Build;
-import android.os.Handler;
-import android.os.Message;
-import android.support.v4.app.FragmentActivity;
+
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.app.NotificationCompat;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
-import android.widget.Toast;
-import android.widget.ViewAnimator;
-
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectInput;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutput;
-import java.io.ObjectOutputStream;
-import java.security.SecureRandom;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
